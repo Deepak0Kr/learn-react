7020a4cf07f5e74f014e412d91e5ef30f8106688
Deepak Kumar <deepkr1099@gmail.com>
2024-02-19 20:55:13 +0000

    #counter project

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
 
 function App() {
+  let [counter,setcountere]=useState(15)
+  
+  const addvalue=()=>{
+    if(counter<20){
+      setcountere(counter+1)
+    }
+  }
+ 
+ 
+  const removeValue =()=>{
+    if(counter>0){
+    setcountere(counter-1)
+    
+    }
+  }
 
   return (
+    <>
     <h1>React with vite | Deepak Kumar</h1>
+    <h2>counter:{counter}</h2>
+    <button onClick={addvalue}>add value</button>
+    <br></br>
+    <button onClick={removeValue}>removeValue</button>
+    </>
   )
 }
 
