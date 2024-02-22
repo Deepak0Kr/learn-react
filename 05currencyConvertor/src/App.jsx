ef7abd7bf48e0d98941038ef1d4826cf712913b8
Deepak Kumar <deepkr1099@gmail.com>
2024-02-22 23:08:29 +0000

    #

---
+++ 05currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-white text-3xl bg-orange-400 '>currency convertor </h1>
+    </>
+  )
+}
+
+export default App
